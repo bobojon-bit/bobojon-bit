1755a1893ebe176962df159d78d7e49d4d8b604c
bobojon-bit <141044009+bobojon-bit@users.noreply.github.com>
2023-08-02 19:56:00 +0500

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,36 @@
+
+# my_list = ["1202","3400","390","1121","3350","1010"]
+# to_int =[]
+
+# for number in my_list:
+    # tu_int.append(int(number))
+
+# to_int  = sorted(to_int)
+# print(to_int)
+
+# to_str =[]
+# for i in to_int:
+ #   to_str.append(str(i))
+
+# print(to_str)
+
+
+import random
+
+names = ["Bobojon","Suman","Siyovush","Akai Umed","Zimfiza","Umedjon"]
+print(random.choice(names))
+
+# my_random = random.randint(10.250)
+# print(my_random)
+
+#print(random.random())
+
+
+names = ["Bobojon","Suman","Siyovush","Akai Umed","Zimfiza","Umedjon"]
+print(random.choice(names))
+
+
+
+
+
+
---
+++ prog8.py
@@ -0,0 +1,13 @@
+### ordinary functions
+def my_func(x):
+    result = x**2
+    return result
+
+#print(my_func (7))
+
+
+#print("------")
+
+# ###  this is lambda function
+#my_calculation = lambda x,y: x*y
+#print(my_calculation(7
